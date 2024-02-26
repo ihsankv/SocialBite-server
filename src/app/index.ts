e5848225f36aa765c0d6eead7c1259ddd54dc156
ihsankv <ihsankv263@gmail.com>
2024-02-26 20:50:40 +0530

    Initial commit

---
+++ src/app/index.ts
@@ -0,0 +1,61 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { User } from './user';
+import cors from 'cors'
+export async function initServer() {
+    const app = express();
+
+    app.use(bodyParser.json())
+    app.use(cors())
+    const graphqlServer = new ApolloServer({
+        typeDefs: `
+           ${User.types}
+    
+            type Query {
+                ${User.queries}
+            }
+    
+        `,
+        resolvers: {
+          Query: {
+            ...User.resolvers.queries,
+          },
+       
+        },
+      });
+    // const graphqlServer = new ApolloServer({
+    //     typeDefs: `
+    //         ${User.types}
+    //             type Query {
+    //                 ${User.queries}
+    //             } 
+    //         `,
+    //     resolvers: {
+    //         Query: {
+    //             ...User.resolvers.queries,
+    //         },
+    //     },
+    // });
+    // const graphqlServer = new ApolloServer({
+    //     typeDefs: `
+    //         type Query {
+    //             sayHello: String
+    //         }
+    //     `,
+    //     resolvers: {
+    //         Query: {
+    //             sayHello: () => 'Hello from Graphql Server'
+    //         },
+    //         // Muatation:{},
+    //     },
+    // });
+
+    await graphqlServer.start();
+
+    app.use('/graphql', expressMiddleware(graphqlServer));
+
+    return app;
+}
+
